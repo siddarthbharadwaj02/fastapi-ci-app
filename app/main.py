104811ad4e3dbc3d6db7f29f08890da71d1a45af
vinay <vinay@pramukha>
2025-07-25 20:25:32 +0530

    Initial FastAPI app

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import os
+
+app = FastAPI()
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "message": "Hello from FastAPI + Jenkins!"})
